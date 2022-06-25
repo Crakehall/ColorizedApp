c822c5b2ab3cbd32094942d0798fd5e0187c6ac9
Oleg Sosnitskiy <orlovolegpr@gmail.com>
2022-06-26 00:14:51 +0300

    Set logic UI

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -18,20 +18,33 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorValue: UILabel!
     @IBOutlet var blueColorValue: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         sliderRedColor.minimumTrackTintColor = .red
         sliderBlueColor.minimumTrackTintColor = .blue
         sliderGreenColor.minimumTrackTintColor = .green
-
+        
     }
     @IBAction func sliderAction(_ sender: UISlider) {
-
+        let color = UIColor(
+            red: CGFloat(sliderRedColor.value),
+            green: CGFloat(sliderGreenColor.value),
+            blue: CGFloat(sliderBlueColor.value),
+            alpha: 1
+        )
+        
+        colorView.backgroundColor = color
+        
+        switch sender.self {
+        case sliderRedColor:
+            redColorValue.text = String(format: "%.2f", sender.value)
+        case sliderGreenColor:
+            greenColorValue.text = String(format: "%.2f", sender.value)
+        default:
+            blueColorValue.text = String(format: "%.2f", sender.value)
         }
         
-    
-
+    }
 }
 
